de85262d1f1f61e937c35ad9e41f2c7b1e44a125
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-05-10 22:39:02 +0200

    Test SQLite

--- app/src/main/java/com/blackghost/phantom/MainActivity.java
+++ app/src/main/java/com/blackghost/phantom/MainActivity.java
@@ -3,6 +3,11 @@ package com.blackghost.phantom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.blackghost.phantom.Managers.DataBaseManager;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DataBaseManager dataBaseManager = new DataBaseManager(this);
+
+        if (dataBaseManager.createDatabase("test.db")) {
+            Log.d("Test", "Database created");
+
+            List<String> databaseNames = dataBaseManager.getDatabaseNames();
+            Log.d("DataBase List", databaseNames.toString());
+        } else {
+            Log.e("Test", "Failed to create database");
+        }
+
     }
 }
--- app/src/main/java/com/blackghost/phantom/Managers/DataBaseManager.java
+++ app/src/main/java/com/blackghost/phantom/Managers/DataBaseManager.java
@@ -1,4 +1,33 @@
 package com.blackghost.phantom.Managers;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataBaseManager {
+    private Context context;
+    private List<String> databaseNames;
+    private static String databaseDirectory = "/databases/";
+
+    public DataBaseManager(Context context) {
+        this.context = context;
+        databaseNames = new ArrayList<>();
+    }
+
+    public boolean createDatabase(String databaseName) {
+        String dbPath = context.getDatabasePath(databaseDirectory + File.separator + databaseName).getPath();
+        SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(dbPath, null);
+        if (db != null) {
+            db.close();
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public List<String> getDatabaseNames() {
+        return databaseNames;
+    }
 }
