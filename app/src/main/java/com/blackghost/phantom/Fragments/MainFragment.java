c5f7cac218a6d34b44709a1ba8b8cd800d43a94e
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-05-13 20:25:27 +0200

    Add github link in MainFragment

--- app/src/main/java/com/blackghost/phantom/Fragments/MainFragment.java
+++ app/src/main/java/com/blackghost/phantom/Fragments/MainFragment.java
@@ -1,5 +1,7 @@
 package com.blackghost.phantom.Fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,11 +9,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.blackghost.phantom.R;
 
 public class MainFragment extends Fragment {
-
+    Button githubBtn;
     public MainFragment() {
         // Required empty public constructor
     }
@@ -24,6 +27,19 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        githubBtn = view.findViewById(R.id.githubBtn);
+
+        githubBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://github.com/BlackGhost0051"));
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
