31b7ef74faaa80ecc9b8b440b694adc1061266d3
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-05-15 09:53:49 +0200

    Add settings.xml and SettingsManager

--- app/src/main/java/com/blackghost/phantom/Fragments/SettingsFragment.java
+++ app/src/main/java/com/blackghost/phantom/Fragments/SettingsFragment.java
@@ -10,6 +10,8 @@ import android.view.ViewGroup;
 
 import com.blackghost.phantom.R;
 
+import java.io.File;
+
 public class SettingsFragment extends Fragment {
     public SettingsFragment() {
         // Required empty public constructor
@@ -23,6 +25,8 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        return view;
     }
 }
