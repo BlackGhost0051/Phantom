cf042fc2a7be329514aa6b15bd59a1e974528548
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-05-16 16:03:51 +0200

    Add MapView

--- app/src/main/java/com/blackghost/phantom/Fragments/CellTowersMapFragment.java
+++ app/src/main/java/com/blackghost/phantom/Fragments/CellTowersMapFragment.java
@@ -10,8 +10,16 @@ import android.view.ViewGroup;
 
 import com.blackghost.phantom.R;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
 public class CellTowersMapFragment extends Fragment {
 
+    private MapView mMap;
+    private IMapController controller;
+    private MyLocationNewOverlay mMyLocationOverlay;
     public CellTowersMapFragment() {
         // Required empty public constructor
     }
@@ -24,6 +32,13 @@ public class CellTowersMapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cell_towers_map, container, false);
+
+        mMap = view.findViewById(R.id.osmmap);
+        controller = mMap.getController();
+
+        controller.setZoom(10.0);
+        controller.setCenter(new GeoPoint(51.5074, -0.1278));
+
         return view;
     }
 }
