d594fde2983207b6029f36666d7fd52242888b60
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-05-17 12:00:44 +0200

    Test SettingsManager

--- app/src/main/java/com/blackghost/phantom/Fragments/SettingsFragment.java
+++ app/src/main/java/com/blackghost/phantom/Fragments/SettingsFragment.java
@@ -4,15 +4,19 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.blackghost.phantom.Managers.SettingsManager;
 import com.blackghost.phantom.R;
 
 import java.io.File;
 
 public class SettingsFragment extends Fragment {
+
+    private SettingsManager settingsManager;
     public SettingsFragment() {
         // Required empty public constructor
     }
@@ -20,13 +24,16 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        settingsManager = new SettingsManager(requireContext());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        boolean test = settingsManager.getBoolSettings("local_data_base", false);
 
+        Log.d("local_data_base",Boolean.toString(test));
         return view;
     }
 }
--- app/src/main/java/com/blackghost/phantom/Managers/SettingsManager.java
+++ app/src/main/java/com/blackghost/phantom/Managers/SettingsManager.java
@@ -1,4 +1,18 @@
 package com.blackghost.phantom.Managers;
-
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 public class SettingsManager {
+    private SharedPreferences sharedPreferences;
+    public SettingsManager(Context context){
+        this.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+    public boolean getBoolSettings(String key, boolean defaultValue){
+        return sharedPreferences.getBoolean(key, defaultValue);
+    }
+    public void setBoolSettings(String key, boolean value) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(key, value);
+        editor.apply();
+    }
 }
