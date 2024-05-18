5255a558c847fa8e0bab352ce5585a02c99b2e5d
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-05-18 20:52:20 +0200

    Change PreferenceFragmentCompat

--- app/src/main/java/com/blackghost/phantom/Fragments/SettingsFragment.java
+++ app/src/main/java/com/blackghost/phantom/Fragments/SettingsFragment.java
@@ -1,39 +1,17 @@
 package com.blackghost.phantom.Fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.blackghost.phantom.Managers.SettingsManager;
+import androidx.preference.PreferenceFragmentCompat;
 import com.blackghost.phantom.R;
+import com.blackghost.phantom.Managers.SettingsManager;
 
-import java.io.File;
-
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
 
     private SettingsManager settingsManager;
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        settingsManager = new SettingsManager(requireContext());
-    }
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_settings, container, false);
-        boolean test = settingsManager.getBoolSettings("local_data_base", false);
-
-        Log.d("local_data_base",Boolean.toString(test));
-        return view;
     }
 }
