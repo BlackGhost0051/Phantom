0272506e9dd56f432b862777ae0997f3b4ff4157
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-05-26 07:26:07 +0200

    Add rotation in map.

--- app/src/main/java/com/blackghost/phantom/Fragments/CellTowersMapFragment.java
+++ app/src/main/java/com/blackghost/phantom/Fragments/CellTowersMapFragment.java
@@ -19,6 +19,8 @@ import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.gestures.RotationGestureDetector;
+import org.osmdroid.views.overlay.gestures.RotationGestureOverlay;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 public class CellTowersMapFragment extends Fragment implements CellTowerInterface {
@@ -49,6 +51,10 @@ public class CellTowersMapFragment extends Fragment implements CellTowerInterfac
 
         controller.setZoom(10.0);
         controller.setCenter(new GeoPoint(51.5074, -0.1278));
+
+        RotationGestureOverlay rotationGestureOverlay = new RotationGestureOverlay(mMap);
+        rotationGestureOverlay.setEnabled(true);
+        mMap.getOverlayManager().add(rotationGestureOverlay);
 
         String bbox = "20.978136062622074,50.01786707355468,20.984798669815067,50.02117598342286";
         cellTowersTask(bbox);
