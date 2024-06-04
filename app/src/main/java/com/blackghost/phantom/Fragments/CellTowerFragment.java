95a6580f456874352cf4e3a3468009a6826972ce
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-06-04 15:22:17 +0200

    Add telephonyManager

--- app/src/main/java/com/blackghost/phantom/Fragments/CellTowerFragment.java
+++ app/src/main/java/com/blackghost/phantom/Fragments/CellTowerFragment.java
@@ -2,7 +2,12 @@ package com.blackghost.phantom.Fragments;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.telephony.CellInfo;
+import android.telephony.CellInfoGsm;
+import android.telephony.CellInfoLte;
+import android.telephony.CellInfoWcdma;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
@@ -14,6 +19,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.blackghost.phantom.R;
+
+import java.util.List;
 
 public class CellTowerFragment extends Fragment {
 
@@ -34,12 +41,13 @@ public class CellTowerFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_cell_tower, container, false);
 
         info_cell_tower = view.findViewById(R.id.info_cell_tower);
-
+        getCellTowerInfo();
 
         return view;
     }
 
 
-    private void getCellTowerInfo(){
+    private void getCellTowerInfo() {
+        TelephonyManager telephonyManager = (TelephonyManager) getActivity().getSystemService(getContext().TELEPHONY_SERVICE);
     }
 }
