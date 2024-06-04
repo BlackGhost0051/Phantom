31e12a15eefdac0f55156c0abbf384057a207d75
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-06-04 14:57:30 +0200

    Mod CellTowerFragment

--- app/src/main/java/com/blackghost/phantom/Fragments/CellTowerFragment.java
+++ app/src/main/java/com/blackghost/phantom/Fragments/CellTowerFragment.java
@@ -1,16 +1,23 @@
 package com.blackghost.phantom.Fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.telecom.TelecomManager;
+import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.blackghost.phantom.R;
 
 public class CellTowerFragment extends Fragment {
+
+    TextView info_cell_tower;
 
     public CellTowerFragment() {
         // Required empty public constructor
@@ -24,6 +31,15 @@ public class CellTowerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_cell_tower, container, false);
+        View view = inflater.inflate(R.layout.fragment_cell_tower, container, false);
+
+        info_cell_tower = view.findViewById(R.id.info_cell_tower);
+
+
+        return view;
+    }
+
+
+    private void getCellTowerInfo(){
     }
 }
