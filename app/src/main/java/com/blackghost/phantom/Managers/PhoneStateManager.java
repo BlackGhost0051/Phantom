97b1d1f07b340f5d6ebcbd17410658d17a31dc33
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-07-24 15:53:58 +0200

    Add pseudocode

--- app/src/main/java/com/blackghost/phantom/Managers/PhoneStateManager.java
+++ app/src/main/java/com/blackghost/phantom/Managers/PhoneStateManager.java
@@ -19,6 +19,8 @@ public class PhoneStateManager extends BroadcastReceiver {
 
         // https://www.opencellid.org/ajax/searchCell.php?mcc=310&mnc=120&lac=21264&cell_id=175545858
         // link to find cell tower in database
+        // return lon lan | make GET box
+        // in box find more info for tower
 
         if (TelephonyManager.EXTRA_STATE_RINGING.equals(state)) {
             String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
