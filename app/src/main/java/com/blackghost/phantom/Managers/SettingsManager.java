0926861777c9615a0417ff072fecc1c342b25c2f
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-07-28 17:37:04 +0200

    Mod SettingsManager

--- app/src/main/java/com/blackghost/phantom/Managers/SettingsManager.java
+++ app/src/main/java/com/blackghost/phantom/Managers/SettingsManager.java
@@ -7,12 +7,5 @@ public class SettingsManager {
     public SettingsManager(Context context){
         this.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
     }
-    public boolean getBoolSettings(String key, boolean defaultValue){
-        return sharedPreferences.getBoolean(key, defaultValue);
-    }
-    public void setBoolSettings(String key, boolean value) {
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putBoolean(key, value);
-        editor.apply();
-    }
+
 }
