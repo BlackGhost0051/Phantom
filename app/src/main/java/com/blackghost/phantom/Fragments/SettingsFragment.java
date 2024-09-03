d2410cdf6a3acca0e9b10012f536cd6cdcc20366
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-09-03 16:58:36 +0200

    Add preferences_style

--- app/src/main/java/com/blackghost/phantom/Fragments/SettingsFragment.java
+++ app/src/main/java/com/blackghost/phantom/Fragments/SettingsFragment.java
@@ -12,6 +12,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
+        getActivity().setTheme(R.style.preferences_style);
     }
 
     @Override
