46ba47d531f8763465ec696555d0dd451c71402b
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-06-14 23:57:21 +0200

    Mod structure

--- app/src/main/java/com/blackghost/phantom/Managers/CellTowerManager.java
+++ app/src/main/java/com/blackghost/phantom/Managers/CellTowerManager.java
@@ -1,13 +1,41 @@
 package com.blackghost.phantom.Managers;
 
+import android.content.Context;
+import android.os.Environment;
 import android.util.Log;
 
 import com.blackghost.phantom.Class.CellTowerTask;
 import com.blackghost.phantom.Interfaces.CellTowerInterface;
+import com.blackghost.phantom.R;
 
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.File;
+
 public class CellTowerManager implements CellTowerInterface {
+
+    private File appFolder;
+
+    public CellTowerManager(Context context){
+        File root = Environment.getExternalStorageDirectory();
+        String folderName = context.getString(R.string.app_name);
+        appFolder = new File(root, folderName);
+
+        if(!appFolder.exists()){
+            boolean created = appFolder.mkdirs();
+
+            if(created){
+
+            } else {
+
+            }
+        } else {
+
+        }
+    }
+
+
 
     public void getCellIdInfo(){
 
@@ -21,8 +49,12 @@ public class CellTowerManager implements CellTowerInterface {
     private void getInfoFromTask(){
 
     }
-    private void getInfoFromCsv(){
+    private void getInfoFromCsv(File file){
+        try{
 
+        }catch(Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
